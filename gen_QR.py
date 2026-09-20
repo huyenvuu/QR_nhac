4729b55ea2820f805d0ac71914ed4ea462d0c9c5
Huyen Vu <huyenmita@gmail.com>
2026-09-20 17:23:48 +0700

    Initial commit

---
+++ gen_QR.py
@@ -0,0 +1,21 @@
+import qrcode  # pyright: ignore[reportMissingModuleSource]
+
+# WEBSITE URL (Thay link Netlify của bạn vào đây)
+URL = "https://random_song.app"
+
+# CREATE QR
+qr = qrcode.QRCode(
+    version=None,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,  # Mức H chịu lỗi cao, thích hợp in kèm logo hoặc dán nhỏ
+    box_size=20,
+    border=4
+)
+
+qr.add_data(URL)
+qr.make(fit=True)
+
+# GENERATE IMAGE
+image = qr.make_image()
+image.save("random_song_qr.png")
+
+print("QR created successfully!")
